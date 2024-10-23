d11fb7837337e5dfe174f32c47943d1190e18ddf
TylerHartwell <thartwell37@gmail.com>
2024-10-23 13:17:14 -0700

    add project type schema

---
+++ schemaTypes/projectType.ts
@@ -0,0 +1,37 @@
+import {defineField, defineType} from 'sanity'
+
+export const projectType = defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'url',
+      type: 'string',
+    }),
+    defineField({
+      name: 'summary',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'blogSlug',
+      type: 'string',
+    }),
+  ],
+})
